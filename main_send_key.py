49223c36cdc793a5197ce2637a487add0b2964bb
oleg.gusev <gusevoleg@gmail.com>
2023-01-17 10:30:16 +0300

    init

---
+++ main_send_key.py
@@ -0,0 +1,15 @@
+import json
+
+import pika
+
+if __name__ == "__main__":
+    parameters = pika.URLParameters("amqp://crawlers:rAt5HbgN9odP@192.168.5.46:5672/crawlers")
+    connection = pika.BlockingConnection(parameters=parameters)
+    channel = connection.channel(channel_number=0)
+    channel.queue_declare(queue='insta_source_parse_key')
+    channel.basic_publish(exchange='',
+                          routing_key='insta_source_parse_key',
+                          body=json.dumps({
+                              "text": "test",
+
+                          }))
