50dc09e2c995390e7f0af3808300689c831fb287
oleg <oleg.gusev@gmail.com>
2023-01-30 18:43:40 +0300

    init

--- add_insta_session.py
+++ add_insta_session.py
@@ -55,7 +55,7 @@ def add_sessions():
             if res.method.message_count < 10:
                 select_sessions = Sessions.objects.filter(
                     Q(last_parsing__isnull=True) | Q(last_parsing__lte=update_time_timezone(
-                        timezone.localtime()) - timedelta(minutes=5)), taken=0, is_active__lte=10, session_id__isnull=False)
+                        timezone.localtime()) - timedelta(minutes=5)), taken=0, is_active__lte=10)
                 proxy_ids = []
                 for session in select_sessions[:100]:
                     proxy_ids.append(session.proxy_id)
