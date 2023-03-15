0172cc28d271b9f827f78ead570c2a32ec6504d1
oleg <oleg.gusev@gmail.com>
2023-03-15 11:53:10 +0300

    update settings

--- update_data.py
+++ update_data.py
@@ -1,6 +1,7 @@
 import os
 import random
 import time
+from django.db.models import Q
 
 
 def update_while():
@@ -11,7 +12,7 @@ def update_while():
         except Exception as e:
             print(f"update_while {e}")
             time.sleep(10)
-        time.sleep(15*60)
+        time.sleep(15 * 60)
 
 
 def update():
@@ -34,17 +35,47 @@ def update():
     from django.utils import timezone
 
     pymysql.install_as_MySQLdb()
-    from core.models import Sessions, Keyword, SourcesItems
+    from core.models import Sessions, Keyword, SourcesItems, Sources, KeywordSource
     django.db.close_old_connections()
+    try:
+        Keyword.objects.filter(network_id=7, taken=1).update(taken=0)
+    except Exception as e:
+        print(f"Keyword update {e}")
+    try:
+        SourcesItems.objects.filter(taken=1, network_id=7).update(taken=0)
+    except Exception as e:
+        print(f"SourcesItems update {e}")
+    try:
 
-    Keyword.objects.filter(network_id=7, taken=1).update(taken=0)
-    SourcesItems.objects.filter(taken=1, network_id=7).update(taken=0)
-    Sessions.objects.filter(is_active__lte=10, taken=1).update(
-        taken=0,
-        start_parsing=update_time_timezone(timezone.localtime()),
-        last_parsing=update_time_timezone(timezone.localtime()),
-    )
-    #
+        Sessions.objects.filter(is_active__lte=10, taken=1).update(
+            taken=0,
+            start_parsing=update_time_timezone(timezone.localtime()),
+            last_parsing=update_time_timezone(timezone.localtime()),
+        )
+    except Exception as e:
+        print(f"Sessions update {e}")
+    try:
+        select_sources = Sources.objects.filter(published=1, status=1)
+
+        key_source = KeywordSource.objects.filter(
+            source_id__in=list(select_sources.values_list('id', flat=True)))
+
+        Keyword.objects.filter(network_id=7, enabled=1, taken=0, disabled=0) \
+            .exclude(id__in=list(key_source.values_list('keyword_id', flat=True))).update(disabled=1)
+    except Exception as e:
+        print(f"key_source update {e}")
+    try:
+        select_sources = Sources.objects.filter(published=1,
+                                                status=1)
+        sources_items = SourcesItems.objects.filter(network_id=7,
+                                                    disabled=0,
+                                                    ).exclude(
+            source_id__in=list(select_sources.values_list('id', flat=True)))
+
+        sources_items.update(disabled=1)
+
+    except Exception as e:
+        print(f"sources_items update {e}")
     # proxy_ids = []
     # for s in Sessions.objects.all():
     #     proxy_ids.append(s.proxy_id)
