b67cd2c72fa093ea2fe74672df8663304f0312f9
oleg.gusev <oleg.gusev@gmail.com>
2023-04-20 13:10:30 +0300

    update proxy

--- add_insta_session.py
+++ add_insta_session.py
@@ -93,8 +93,8 @@ def add_sessions():
                         session.start_parsing = update_time_timezone(timezone.localtime())
                         sessions_id.append(session)
                     except Exception as e:
+                        session.error_message = f"proxy {session.proxy_id} {str(e)}"
                         session.proxy_id = None
-                        session.error_message = str(e)
                         session.save()
                 Sessions.objects.bulk_update(sessions_id, ['taken', 'start_parsing'],
                                              batch_size=200)
