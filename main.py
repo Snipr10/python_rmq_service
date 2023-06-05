ea15e76bb279314e9452ed9e2fca787f6f846c6a
oleg.gusev <oleg.gusev@gmail.com>
2023-06-05 16:57:30 +0300

    added priority

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ if __name__ == '__main__':
     threading.Thread(target=read_reslut_while, args=()).start()
 
     print("update_session_id_while")
-    threading.Thread(target=update_session_id_while, args=()).start()
+    # threading.Thread(target=update_session_id_while, args=()).start()
