8490d90dff0e052827280c2f54a4f36d6e04a8a6
oleg.gusev <oleg.gusev@gmail.com>
2023-07-19 14:12:02 +0300

    added test proxy

--- add_insta_session.py
+++ add_insta_session.py
@@ -66,7 +66,7 @@ def add_sessions():
                         timezone.localtime()) - timedelta(minutes=5)),
                     Q(start_parsing__isnull=True) | Q(start_parsing__lte=update_time_timezone(
                         timezone.localtime()) - timedelta(minutes=5)),
-                    taken=0, is_active__lte=10, settings__isnull=False).exclude(proxy_id__isnull=True, id__lte=5770).order_by("last_parsing")
+                    taken=0, is_active__lte=10, settings__isnull=False).exclude(proxy_id__isnull=True, id__gte=5770).order_by("last_parsing")
                 proxy_ids = []
                 for session in select_sessions[:100]:
                     proxy_ids.append(session.proxy_id)
