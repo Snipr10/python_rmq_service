f9c6f590e134ddc35aad81ad173750f09610847c
oleg.gusev <oleg.gusev@gmail.com>
2023-07-19 17:43:25 +0300

    added test proxy

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from update_data import update_while
 
 
 if __name__ == '__main__':
-    #
-    # print("update_while")
-    # threading.Thread(target=update_while, args=()).start()
+
+    print("update_while")
+    threading.Thread(target=update_while, args=()).start()
     #
     print("add_keys")
     threading.Thread(target=add_keys_while, args=()).start()
